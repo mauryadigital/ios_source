0d31d993b56fb6c7ba8ee96f39324386cf76137e
User <user@Users-MacBook-Pro.local>
2016-07-21 19:04:49 +0530

    integrated change password

---
+++ LeaveManagement/ChangePasswordsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChangePasswordsViewController.h
+//  LeaveManagement
+//
+//  Created by User on 7/19/16.
+//  Copyright © 2016 Pyramid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChangePasswordsViewController : UIViewController
+
+@end
---
+++ LeaveManagement/ChangePasswordsViewController.m
@@ -0,0 +1,37 @@
+//
+//  ChangePasswordsViewController.m
+//  LeaveManagement
+//
+//  Created by User on 7/19/16.
+//  Copyright © 2016 Pyramid. All rights reserved.
+//
+
+#import "ChangePasswordsViewController.h"
+
+@interface ChangePasswordsViewController ()
+
+@end
+
+@implementation ChangePasswordsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
